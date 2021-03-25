3dd7563894bc975e762488dfbe72fa37de481651
GelNiz <angelinanizam@gmail.com>
2021-03-25 18:38:48 +0500

    finished practice3

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name = 'function',
+    version = '0.0.1',
+    author = 'AngelNiz',
+    author_email = 'angelinanizam@gmail.com',
+    description = 'Task with creation of package from practice 3',
+    packages = setuptools.find_packages()
+
+)
